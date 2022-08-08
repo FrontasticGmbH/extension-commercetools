436697ff16c91e2fa4df9457bbc8e164f4030e34
Carlos Mourullo <carlosmourullo@gmail.com>
2022-08-08 10:37:54 +0200

    feat(FP-2155): implemented error type on Wishlist API

--- apis/WishlistApi.ts
+++ apis/WishlistApi.ts
@@ -1,6 +1,7 @@
 import { BaseApi } from './BaseApi';
 import { WishlistMapper } from '../mappers/WishlistMapper';
 import { Wishlist } from '../../../types/wishlist/Wishlist';
+import { ExternalError } from '../utils/Errors';
 
 const expandVariants = ['lineItems[*].variant'];
 
@@ -11,171 +12,171 @@ interface AddToWishlistRequest {
 
 export class WishlistApi extends BaseApi {
   getById = async (wishlistId: string) => {
-    try {
-      const locale = await this.getCommercetoolsLocal();
-      const response = await this.getApiForProject()
-        .shoppingLists()
-        .withId({ ID: wishlistId })
-        .get({
-          queryArgs: {
-            expand: expandVariants,
-          },
-        })
-        .execute();
-
-      return WishlistMapper.commercetoolsShoppingListToWishlist(response.body, locale);
-    } catch (error) {
-      throw new Error(`Get wishlist by ID failed: ${error}`);
-    }
+    const locale = await this.getCommercetoolsLocal();
+    return await this.getApiForProject()
+      .shoppingLists()
+      .withId({ ID: wishlistId })
+      .get({
+        queryArgs: {
+          expand: expandVariants,
+        },
+      })
+      .execute()
+      .then(response => {
+        return WishlistMapper.commercetoolsShoppingListToWishlist(response.body, locale);
+      })
+      .catch(error => {
+        throw new ExternalError({ status: error.code, message: error.message, body: error.body });
+      });
   };
 
   getForAccount = async (accountId: string) => {
-    try {
-      const locale = await this.getCommercetoolsLocal();
-      const response = await this.getApiForProject()
-        .shoppingLists()
-        .get({
-          queryArgs: {
-            where: `customer(id="${accountId}")`,
-            expand: expandVariants,
-          },
-        })
-        .execute();
-
-      return response.body.results.map((shoppingList) =>
-        WishlistMapper.commercetoolsShoppingListToWishlist(shoppingList, locale),
-      );
-    } catch (error) {
-      throw new Error(`Get wishlist for account failed: ${error}`);
-    }
+    const locale = await this.getCommercetoolsLocal();
+    return await this.getApiForProject()
+      .shoppingLists()
+      .get({
+        queryArgs: {
+          where: `customer(id="${accountId}")`,
+          expand: expandVariants,
+        },
+      })
+      .execute()
+      .then(response => {
+        return response.body.results.map(shoppingList =>
+          WishlistMapper.commercetoolsShoppingListToWishlist(shoppingList, locale),
+        );
+      })
+      .catch(error => {
+        throw new ExternalError({ status: error.code, message: error.message, body: error.body });
+      });
   };
 
   getByIdForAccount = async (wishlistId: string, accountId: string) => {
-    try {
-      const locale = await this.getCommercetoolsLocal();
-      const response = await this.getApiForProject()
-        .shoppingLists()
-        .withId({ ID: wishlistId })
-        .get({
-          queryArgs: {
-            where: `customer(id="${accountId}")`,
-            expand: expandVariants,
-          },
-        })
-        .execute();
-
-      return WishlistMapper.commercetoolsShoppingListToWishlist(response.body, locale);
-    } catch (error) {
-      throw new Error(`Get wishlist by ID failed: ${error}`);
-    }
+    const locale = await this.getCommercetoolsLocal();
+    return await this.getApiForProject()
+      .shoppingLists()
+      .withId({ ID: wishlistId })
+      .get({
+        queryArgs: {
+          where: `customer(id="${accountId}")`,
+          expand: expandVariants,
+        },
+      })
+      .execute()
+      .then(response => {
+        return WishlistMapper.commercetoolsShoppingListToWishlist(response.body, locale);
+      })
+      .catch(error => {
+        throw new ExternalError({ status: error.code, message: error.message, body: error.body });
+      });
   };
 
   create = async (wishlist: Omit<Wishlist, 'wishlistId'>) => {
-    try {
-      const locale = await this.getCommercetoolsLocal();
-      const body = WishlistMapper.wishlistToCommercetoolsShoppingListDraft(wishlist, locale);
-      const response = await this.getApiForProject()
-        .shoppingLists()
-        .post({
-          body: body,
-          queryArgs: {
-            expand: expandVariants,
-          },
-        })
-        .execute();
-
-      return WishlistMapper.commercetoolsShoppingListToWishlist(response.body, locale);
-    } catch (error) {
-      throw new Error(`Create wishlist failed: ${error}`);
-    }
+    const locale = await this.getCommercetoolsLocal();
+    const body = WishlistMapper.wishlistToCommercetoolsShoppingListDraft(wishlist, locale);
+    return await this.getApiForProject()
+      .shoppingLists()
+      .post({
+        body: body,
+        queryArgs: {
+          expand: expandVariants,
+        },
+      })
+      .execute()
+      .then(response => {
+        return WishlistMapper.commercetoolsShoppingListToWishlist(response.body, locale);
+      })
+      .catch(error => {
+        throw new ExternalError({ status: error.code, message: error.message, body: error.body });
+      });
   };
 
   addToWishlist = async (wishlist: Wishlist, request: AddToWishlistRequest) => {
-    try {
-      const locale = await this.getCommercetoolsLocal();
+    const locale = await this.getCommercetoolsLocal();
 
-      const response = await this.getApiForProject()
-        .shoppingLists()
-        .withId({ ID: wishlist.wishlistId })
-        .post({
-          body: {
-            version: +wishlist.wishlistVersion,
-            actions: [
-              {
-                action: 'addLineItem',
-                sku: request.sku,
-                quantity: request.count,
-              },
-            ],
-          },
-          queryArgs: {
-            expand: expandVariants,
-          },
-        })
-        .execute();
-
-      return WishlistMapper.commercetoolsShoppingListToWishlist(response.body, locale);
-    } catch (error) {
-      throw new Error(`Add to wishlist failed: ${error}`);
-    }
+    return await this.getApiForProject()
+      .shoppingLists()
+      .withId({ ID: wishlist.wishlistId })
+      .post({
+        body: {
+          version: +wishlist.wishlistVersion,
+          actions: [
+            {
+              action: 'addLineItem',
+              sku: request.sku,
+              quantity: request.count,
+            },
+          ],
+        },
+        queryArgs: {
+          expand: expandVariants,
+        },
+      })
+      .execute()
+      .then(response => {
+        return WishlistMapper.commercetoolsShoppingListToWishlist(response.body, locale);
+      })
+      .catch(error => {
+        throw new ExternalError({ status: error.code, message: error.message, body: error.body });
+      });
   };
 
   removeLineItem = async (wishlist: Wishlist, lineItemId: string) => {
-    try {
-      const locale = await this.getCommercetoolsLocal();
+    const locale = await this.getCommercetoolsLocal();
 
-      const response = await this.getApiForProject()
-        .shoppingLists()
-        .withId({ ID: wishlist.wishlistId })
-        .post({
-          body: {
-            version: +wishlist.wishlistVersion,
-            actions: [
-              {
-                action: 'removeLineItem',
-                lineItemId,
-              },
-            ],
-          },
-          queryArgs: {
-            expand: expandVariants,
-          },
-        })
-        .execute();
-
-      return WishlistMapper.commercetoolsShoppingListToWishlist(response.body, locale);
-    } catch (error) {
-      throw new Error(`Add to wishlist failed: ${error}`);
-    }
+    return await this.getApiForProject()
+      .shoppingLists()
+      .withId({ ID: wishlist.wishlistId })
+      .post({
+        body: {
+          version: +wishlist.wishlistVersion,
+          actions: [
+            {
+              action: 'removeLineItem',
+              lineItemId,
+            },
+          ],
+        },
+        queryArgs: {
+          expand: expandVariants,
+        },
+      })
+      .execute()
+      .then(response => {
+        return WishlistMapper.commercetoolsShoppingListToWishlist(response.body, locale);
+      })
+      .catch(error => {
+        throw new ExternalError({ status: error.code, message: error.message, body: error.body });
+      });
   };
 
   updateLineItemCount = async (wishlist: Wishlist, lineItemId: string, count: number) => {
-    try {
-      const locale = await this.getCommercetoolsLocal();
+    const locale = await this.getCommercetoolsLocal();
 
-      const response = await this.getApiForProject()
-        .shoppingLists()
-        .withId({ ID: wishlist.wishlistId })
-        .post({
-          body: {
-            version: +wishlist.wishlistVersion,
-            actions: [
-              {
-                action: 'changeLineItemQuantity',
-                lineItemId,
-                quantity: count,
-              },
-            ],
-          },
-          queryArgs: {
-            expand: expandVariants,
-          },
-        })
-        .execute();
-
-      return WishlistMapper.commercetoolsShoppingListToWishlist(response.body, locale);
-    } catch (error) {
-      throw new Error(`Update line item count: ${error}`);
-    }
+    return await this.getApiForProject()
+      .shoppingLists()
+      .withId({ ID: wishlist.wishlistId })
+      .post({
+        body: {
+          version: +wishlist.wishlistVersion,
+          actions: [
+            {
+              action: 'changeLineItemQuantity',
+              lineItemId,
+              quantity: count,
+            },
+          ],
+        },
+        queryArgs: {
+          expand: expandVariants,
+        },
+      })
+      .execute()
+      .then(response => {
+        return WishlistMapper.commercetoolsShoppingListToWishlist(response.body, locale);
+      })
+      .catch(error => {
+        throw new ExternalError({ status: error.code, message: error.message, body: error.body });
+      });
   };
 }
