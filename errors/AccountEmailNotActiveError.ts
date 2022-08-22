def141ccf28d4958316d530afbffa08002b3c51d
Carlos Mourullo <carlosmourullo@gmail.com>
2022-08-22 13:55:45 +0200

    feat(FP-2402): removed unused error type

--- errors/AccountEmailNotActiveError.ts
+++
@@ -1,8 +0,0 @@
-import { ErrorProps, ExtensionError } from '../utils/Errors';
-
-export class AccountEmailNotActiveError extends ExtensionError {
-  constructor(options: ErrorProps) {
-    super(options);
-    this.code = 'account_email_not_active_error';
-  }
-}
