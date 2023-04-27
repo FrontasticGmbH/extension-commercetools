068717c51ce84cabff0cdf79e773f711053d18fd
Joel Fickson <joelngozo@gmail.com>
2023-04-27 23:47:45 +0200

    feat[fp:2575]: update data sources to return preview payload

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
-import * as AccountActions from './actionControllers/AccountController';
-import * as ProductActions from './actionControllers/ProductController';
-import * as CartActions from './actionControllers/CartController';
-import * as WishlistActions from './actionControllers/WishlistController';
-import * as ProjectActions from './actionControllers/ProjectController';
+import * as AccountActions from './actionControllers/AccountController'
+import * as ProductActions from './actionControllers/ProductController'
+import * as CartActions from './actionControllers/CartController'
+import * as WishlistActions from './actionControllers/WishlistController'
+import * as ProjectActions from './actionControllers/ProjectController'
+import { DataSourcePreviewPayloadElement } from '@frontastic/extension-types/src/ts'
 
 import {
   DataSourceConfiguration,
@@ -12,16 +13,16 @@ import {
   DynamicPageSuccessResult,
   ExtensionRegistry,
   Request,
-} from '@frontastic/extension-types';
-import { getLocale, getPath } from './utils/Request';
-import { ProductRouter } from './utils/ProductRouter';
-import { Product } from '@Types/product/Product';
-import { SearchRouter } from './utils/SearchRouter';
-import { Result } from '@Types/product/Result';
-import { CategoryRouter } from './utils/CategoryRouter';
-import { ProductApi } from './apis/ProductApi';
-import { ProductQueryFactory } from './utils/ProductQueryFactory';
-import { ValidationError } from './utils/Errors';
+} from '@frontastic/extension-types'
+import { getLocale, getPath } from './utils/Request'
+import { ProductRouter } from './utils/ProductRouter'
+import { Product } from '@Types/product/Product'
+import { SearchRouter } from './utils/SearchRouter'
+import { Result } from '@Types/product/Result'
+import { CategoryRouter } from './utils/CategoryRouter'
+import { ProductApi } from './apis/ProductApi'
+import { ProductQueryFactory } from './utils/ProductQueryFactory'
+import { ValidationError } from './utils/Errors'
 
 export default {
   'dynamic-page-handler': async (
@@ -31,13 +32,13 @@ export default {
     // Identify static page
     const staticPageMatch = getPath(request)?.match(
       /^\/(cart|checkout|wishlist|account|login|register|reset-password|thank-you)$/,
-    );
+    )
     if (staticPageMatch) {
       return {
         dynamicPageType: `frontastic${staticPageMatch[0]}`,
         dataSourcePayload: {},
         pageMatchingPayload: {},
-      } as DynamicPageSuccessResult;
+      } as DynamicPageSuccessResult
     }
 
     // Identify Product
@@ -52,12 +53,12 @@ export default {
             pageMatchingPayload: {
               product: product,
             },
-          };
+          }
         }
 
         // FIXME: Return proper error result
-        return null;
-      });
+        return null
+      })
     }
 
     // Identify Search
@@ -68,12 +69,12 @@ export default {
             dynamicPageType: 'frontastic/search',
             dataSourcePayload: result,
             pageMatchingPayload: result,
-          };
+          }
         }
 
         // FIXME: Return proper error result
-        return null;
-      });
+        return null
+      })
     }
 
     if (CategoryRouter.identifyFrom(request)) {
@@ -83,75 +84,124 @@ export default {
             dynamicPageType: 'frontastic/category',
             dataSourcePayload: result,
             pageMatchingPayload: result,
-          };
+          }
         }
 
         // FIXME: Return proper error result
-        return null;
-      });
+        return null
+      })
     }
 
-    return null;
+    return null
   },
   'data-sources': {
     'frontastic/product-list': async (config: DataSourceConfiguration, context: DataSourceContext) => {
-      const locale = context.request ? getLocale(context.request) : null;
+      const locale = context.request ? getLocale(context.request) : null
 
-      const productApi = new ProductApi(context.frontasticContext, locale);
-      const productQuery = ProductQueryFactory.queryFromParams(context?.request, config);
+      const productApi = new ProductApi(context.frontasticContext, locale)
+      const productQuery = ProductQueryFactory.queryFromParams(context?.request, config)
       return await productApi.query(productQuery).then((queryResult) => {
+
+        if (!context.isPreview) {
+          return {
+            dataSourcePayload: queryResult,
+          }
+        }
+
         return {
           dataSourcePayload: queryResult,
-        };
-      });
+          previewPayload: (queryResult.items as Product[]).map((product): DataSourcePreviewPayloadElement => {
+            return {
+              title: product.name,
+              image: product?.variants[0]?.images[0],
+            }
+          }),
+        }
+      })
     },
 
     'frontastic/similar-products': async (config: DataSourceConfiguration, context: DataSourceContext) => {
       if (!context.hasOwnProperty('request')) {
         throw new ValidationError({
           message: `Request is not defined in context ${context}`,
-        });
+        })
       }
 
-      const locale = context.request ? getLocale(context.request) : null;
+      const locale = context.request ? getLocale(context.request) : null
 
-      const productApi = new ProductApi(context.frontasticContext, locale);
-      const productQuery = ProductQueryFactory.queryFromParams(context.request, config);
+      const productApi = new ProductApi(context.frontasticContext, locale)
+      const productQuery = ProductQueryFactory.queryFromParams(context.request, config)
       const query = {
         ...productQuery,
         categories: [
           (context.pageFolder.dataSourceConfigurations.find((stream) => (stream as any).streamId === '__master') as any)
             ?.preloadedValue?.product?.categories?.[0]?.categoryId,
         ],
-      };
+      }
 
       return await productApi.query(query).then((queryResult) => {
+
+        if (!context.isPreview) {
+          return {
+            dataSourcePayload: queryResult,
+          }
+        }
+
         return {
           dataSourcePayload: queryResult,
-        };
-      });
+          previewPayload: (queryResult.items as Product[]).map((product): DataSourcePreviewPayloadElement => {
+            return {
+              title: product.name,
+              image: product?.variants[0]?.images[0],
+            }
+          }),
+        }
+      })
     },
 
     'frontastic/product': async (config: DataSourceConfiguration, context: DataSourceContext) => {
-      const locale = context.request ? getLocale(context.request) : null;
+      const locale = context.request ? getLocale(context.request) : null
 
-      const productApi = new ProductApi(context.frontasticContext, locale);
+      const productApi = new ProductApi(context.frontasticContext, locale)
 
-      const productQuery = ProductQueryFactory.queryFromParams(context?.request, config);
+      const productQuery = ProductQueryFactory.queryFromParams(context?.request, config)
 
       return await productApi.getProduct(productQuery).then((queryResult) => {
+
+        if (!context.isPreview) {
+          return {
+            dataSourcePayload: {
+              product: queryResult,
+            },
+          }
+        }
+
         return {
           dataSourcePayload: {
             product: queryResult,
           },
-        };
-      });
+          previewPayload: [
+            {
+              title: queryResult.name,
+              image: queryResult?.variants[0]?.images[0],
+            },
+          ],
+        }
+      })
     },
 
     'frontastic/empty': async (config: DataSourceConfiguration, context: DataSourceContext) => {
+
+      if (!context.isPreview) {
+        return {
+          dataSourcePayload: {},
+        }
+      }
+
       return {
         dataSourcePayload: {},
-      };
+        previewPayload: [],
+      }
     },
   },
   actions: {
@@ -161,4 +211,4 @@ export default {
     wishlist: WishlistActions,
     project: ProjectActions,
   },
-} as ExtensionRegistry;
+} as ExtensionRegistry
