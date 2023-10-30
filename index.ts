0c6111131c87f888245eace3e50c4987d8bedd11
Carlos Mourullo <carlosmourullo@gmail.com>
2023-10-30 16:21:41 +0100

    chore restore data-source handler for frontastic/other-products

--- index.ts
+++ index.ts
@@ -177,6 +177,40 @@ export default {
       });
     },
 
+    'frontastic/other-products': async (config: DataSourceConfiguration, context: DataSourceContext) => {
+      if (!context.hasOwnProperty('request')) {
+        throw new ValidationError({
+          message: `Request is not defined in context ${context}`,
+        });
+      }
+
+      const productApi = new ProductApi(
+        context.frontasticContext,
+        getLocale(context.request),
+        getCurrency(context.request),
+      );
+      const productQuery = ProductQueryFactory.queryFromParams(context.request, config);
+
+      const shuffleArray = (array: any) => {
+        for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          const temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+        }
+        return array;
+      };
+
+      return await productApi.query(productQuery).then((queryResult) => {
+        return {
+          dataSourcePayload: {
+            ...queryResult,
+            items: shuffleArray(queryResult.items),
+          },
+        };
+      });
+    },
+
     'frontastic/empty': async (config: DataSourceConfiguration, context: DataSourceContext) => {
       return !context.isPreview
         ? { dataSourcePayload: {} }
